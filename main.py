e112bd804348a06f138a6ff77b095382eb7c1858
levtsn <117160989+levtsn@users.noreply.github.com>
2022-11-03 12:30:24 +0300

    add Piece

--- main.py
+++ main.py
@@ -1,35 +1,31 @@
 import pygame
 
 
+# стакан
 
-
-#стакан
-
-def list2d(max,may):
-    buf=list()
+def list2d(max, may):
+    buf = list()
     for i in range(may):
         buf.append([])
         for j in range(max):
-            buf[i].append(i*100+j)
+            buf[i].append(i * 100 + j)
     return buf
 
-FieldMax=10
-FieldMay=20
 
-Field=list2d(FieldMax,FieldMay)
-print (Field)
+FieldMax = 10
+FieldMay = 20
 
-#фигура
-PieceMax=4
-PieceMay=4
-Piece=list2d(PieceMax,PieceMay)
-PieceX=0
-PieceY=0
-PieceR=0 #Rotation 0..3
+Field = list2d(FieldMax, FieldMay)
+print(Field)
 
-print (Piece)
+# фигура
+PieceMax = 4
+PieceMay = 4
 
+PieceX = 0
+PieceY = 0
+PieceR = 0  # Rotation 0..3
 
-
-print (s)
-print (s[1][1])
+#Piece = list2d(PieceMax, PieceMay)
+Piece = list2d(PieceMax, PieceMay)
+print(Piece)
