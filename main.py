00eaccb988491adbaf64074aed85ca3c77a37e84
levtsn <117160989+levtsn@users.noreply.github.com>
2022-11-03 12:47:41 +0300

    add collision modify list2d

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import pygame
 
 # стакан
 
-def list2d(max, may):
+def list2d(max, may, value=0):
     buf = list()
     for i in range(may):
         buf.append([])
         for j in range(max):
-            buf[i].append(i * 100 + j)
+            buf[i].append(value)
     return buf
 
 
@@ -29,3 +29,16 @@ PieceR = 0  # Rotation 0..3
 #Piece = list2d(PieceMax, PieceMay)
 Piece = list2d(PieceMax, PieceMay)
 print(Piece)
+
+def CollisionCheck():
+    for x in range(PieceMax):
+        for y in range(PieceMay):
+            cx=x+PieceX
+            cy=y+PieceY
+            if cx in range(FieldMax) and cy in range (FieldMay):
+                if Piece[x][y]!=0 and Field[cx][cy]!=0:
+                    return True
+    return False
+PieceX=0
+PieceY=0
+print (CollisionCheck())
